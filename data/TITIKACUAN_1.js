19bfa71c1fdf42d6ad978707abf94ef8b618473e
iqbalandras <75374076+iqbalandras@users.noreply.github.com>
2020-12-07 21:31:32 +0700

    Add files via upload

---
+++ data/TITIKACUAN_1.js
@@ -0,0 +1,9 @@
+var json_TITIKACUAN_1 = {
+"type": "FeatureCollection",
+"name": "TITIKACUAN_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 109.661687230873554, -6.89153653956491 ] } },
+{ "type": "Feature", "properties": { "id": "2" }, "geometry": { "type": "Point", "coordinates": [ 109.672150905223901, -6.878342403646773 ] } }
+]
+}
